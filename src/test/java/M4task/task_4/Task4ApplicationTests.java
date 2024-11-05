f9e9954fc9282b23e8974b60bed52adfd99e5f1a
Леонид Мартов <leonidmartov@MacBook-Air-Leonid.local>
2024-11-05 22:52:09 +0300

    done

---
+++ src/test/java/M4task/task_4/Task4ApplicationTests.java
@@ -0,0 +1,13 @@
+package M4task.task_4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Task4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
